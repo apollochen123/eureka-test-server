5ccd7863d8b18c30e3781ad9ce55fd3ded447d59
apollochen123 <apollo.chan@qq.com>
2017-07-29 16:43:42 +0800

    lastTest
    Change-Id: I43caa9b5263a2e258d71388554bc82c9eb451a99

--- src/main/java/com/chen/eurika/Server/EurekaTestServerApplication.java
+++ src/main/java/com/chen/eurika/Server/EurekaTestServerApplication.java
@@ -7,6 +7,7 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 @SpringBootApplication
 @EnableEurekaServer
 public class EurekaTestServerApplication {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaTestServerApplication.class, args);
 	}
