0afcdba0b341f27b8117476a37c5d2f903296e4b
apollochen123 <apollo.chan@qq.com>
2017-07-29 17:35:21 +0800

    1735
    Change-Id: I6d7bbdabfecffe8ee4935104381d0ab9dc852c9c

--- src/main/java/com/chen/eurika/Server/EurekaTestServerApplication.java
+++ src/main/java/com/chen/eurika/Server/EurekaTestServerApplication.java
@@ -11,5 +11,6 @@ public class EurekaTestServerApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaTestServerApplication.class, args);
 		System.out.println("1728");
+		System.out.println("1735");
 	}
 }
